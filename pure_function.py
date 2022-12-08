14aee49652517651a9511a833db9661f3db70fb5
mdmafuz2022 <golam.mafuz.work2@gmail.com>
2022-12-08 21:54:31 +0530

    Map, Filter and Comprehensions

---
+++ pure_function.py
@@ -0,0 +1,9 @@
+"""
+Functional Programming Concepts :
+
+Functional languages are declarative languages, they tell the computer what
+result they want. This is usually contrasted with imperative languages that
+tell the computer what steps to take to solve a problem. Python is usually
+coded in an imperative way but can use the declarative style if necessary.
+
+"""
